74487c53300cc8121c837b6619a9b9d08c37b12a
divyanshu <divyanshu.pundir@gmail.com>
2022-05-11 12:02:35 +0530

    build: Added gradle plugin publish workflow

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -1,16 +1,26 @@
 plugins {
     kotlin("jvm")
     `java-gradle-plugin`
+    id("maven-publish")
+    id("com.gradle.plugin-publish") version "0.18.0"
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
 
+pluginBundle {
+    website = "https://github.com/urbanmatrix/mavlink-kotlin"
+    vcsUrl = "https://github.com/urbanmatrix/mavlink-kotlin"
+    tags = listOf("mavlink", "kotlin", "jvm", "generator")
+}
+
 gradlePlugin {
     plugins {
         create("mavlinkGenerator") {
             id = "com.urbanmatrix.mavlink.generator"
+            displayName = "Mavlink Kotlin Generator"
+            description = "Mavlink Kotlin Generator"
             implementationClass = "com.urbanmatrix.mavlink.generator.MavlinkGeneratorPlugin"
         }
     }
