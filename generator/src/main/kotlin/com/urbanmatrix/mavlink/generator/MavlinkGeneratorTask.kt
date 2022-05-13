702772a7446417d50204e8fff82036a918d902f3
divyanshu <divyanshu.pundir@gmail.com>
2022-05-13 10:47:20 +0530

    feat: Simple inputs for MavlinkGeneratorTask

--- generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/MavlinkGeneratorTask.kt
+++ generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/MavlinkGeneratorTask.kt
@@ -1,5 +1,23 @@
 package com.urbanmatrix.mavlink.generator
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+import java.io.File
 
-class MavlinkGeneratorTask : DefaultTask()
+class MavlinkGeneratorTask : DefaultTask() {
+
+    @OutputDirectory
+    var generatedSourcesDir: File? = null
+
+    private val definitions = mutableListOf<File>()
+
+    fun include(file: File){
+        definitions.add(file)
+    }
+
+    @TaskAction
+    fun generate() {
+
+    }
+}
