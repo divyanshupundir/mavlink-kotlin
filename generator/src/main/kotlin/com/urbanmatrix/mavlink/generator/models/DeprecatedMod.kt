edd979811e52a83141bacdc230c54d6d6a4722c0
divyanshu <divyanshu.pundir@gmail.com>
2022-05-19 16:35:03 +0530

    feat: Created DeprecatedXml

--- generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/models/DeprecatedMod.kt
+++ generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/models/DeprecatedMod.kt
@@ -1,7 +1,23 @@
 package com.urbanmatrix.mavlink.generator.models
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
+
 class DeprecatedMod(
     val since: String,
     val replacedBy: String,
     val message: String?
 )
+
+@JacksonXmlRootElement(localName = "deprecated")
+class DeprecatedXml {
+
+    @JacksonXmlProperty(localName = "since", isAttribute = true)
+    var since: String? = null
+
+    @JacksonXmlProperty(localName = "replaced_by", isAttribute = true)
+    var replacedBy: String? = null
+
+    @JacksonXmlProperty(isAttribute = false)
+    var message: String? = null
+}
