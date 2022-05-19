332e6a11f18abc907629fde1f3d21b371f53ad5d
divyanshu <divyanshu.pundir@gmail.com>
2022-05-19 16:24:04 +0530

    build: Set kotlin and java version for buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,3 +5,14 @@ plugins {
 repositories {
     mavenCentral()
 }
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
