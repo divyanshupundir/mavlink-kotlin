4caa5134c643951857191ae25f6357c0ef36a308
divyanshu <divyanshu.pundir@gmail.com>
2022-06-07 16:36:13 +0530

    feat: Handling EnumMod deserialization

--- generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/models/EnumMod.kt
+++ generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/models/EnumMod.kt
@@ -1,8 +1,41 @@
 package com.urbanmatrix.mavlink.generator.models
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
+
 data class EnumMod(
     val name: String,
     val entries: List<EnumEntryMod>,
     val deprecated: DeprecatedMod?,
     val description: String?
 )
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JacksonXmlRootElement(localName = "enum")
+data class EnumXml(
+
+    @JacksonXmlProperty(localName = "name", isAttribute = true)
+    val name: String,
+
+    @JacksonXmlProperty(localName = "deprecated")
+    val deprecated: DeprecatedXml?,
+
+    @JacksonXmlProperty(localName = "description")
+    val description: String?
+) {
+    val entries = mutableListOf<EnumEntryXml>()
+
+    @JsonSetter("entry")
+    fun addEnumEntry(enumEntry: EnumEntryXml) {
+        entries += enumEntry
+    }
+
+    fun toMod() = EnumMod(
+        name,
+        entries.map { it.toMod() },
+        deprecated?.toMod(),
+        description
+    )
+}
