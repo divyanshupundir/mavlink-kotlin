293b42744e34241e8d406a5e4e3d17efcf769195
divyanshu <divyanshu.pundir@gmail.com>
2022-06-07 16:24:00 +0530

    feat: Handling EnumEntryMod deserialization

--- generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/models/EnumEntryMod.kt
+++ generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/models/EnumEntryMod.kt
@@ -1,5 +1,11 @@
 package com.urbanmatrix.mavlink.generator.models
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
+
 data class EnumEntryMod(
     val value: Int,
     val name: String,
@@ -7,3 +13,35 @@ data class EnumEntryMod(
     val deprecated: DeprecatedMod?,
     val description: String?
 )
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JacksonXmlRootElement(localName = "entry")
+data class EnumEntryXml(
+
+    @JacksonXmlProperty(localName = "value", isAttribute = true)
+    val value: Int,
+
+    @JacksonXmlProperty(localName = "name", isAttribute = true)
+    val name: String,
+
+    @JacksonXmlProperty(localName = "deprecated")
+    val deprecated: DeprecatedXml?,
+
+    @JacksonXmlProperty(localName = "description")
+    val description: String?
+) {
+    val params = mutableListOf<ParamXml>()
+
+    @JsonSetter("param")
+    fun addParam(param: ParamXml) {
+        params += param
+    }
+
+    fun toMod() = EnumEntryMod(
+        value,
+        name,
+        params.map { it.toMod() },
+        deprecated?.toMod(),
+        description
+    )
+}
