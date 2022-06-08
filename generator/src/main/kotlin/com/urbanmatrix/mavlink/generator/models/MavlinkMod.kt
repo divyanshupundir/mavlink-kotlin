85ce25e4358bf380189358f4a239a228dfc0ad01
divyanshu <divyanshu.pundir@gmail.com>
2022-06-08 09:54:39 +0530

    feat: Handling Mavlink deserialization

--- generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/models/MavlinkMod.kt
+++ generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/models/MavlinkMod.kt
@@ -1,5 +1,11 @@
 package com.urbanmatrix.mavlink.generator.models
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
+
 data class MavlinkMod(
     val name: String,
     val includes: List<String>,
@@ -8,3 +14,36 @@ data class MavlinkMod(
     val enums: List<EnumModel>,
     val messages: List<MessageModel>
 )
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JacksonXmlRootElement(localName = "mavlink")
+data class MavlinkXml(
+
+    @JacksonXmlProperty(localName = "version", isAttribute = true)
+    val version: Int,
+
+    @JacksonXmlProperty(localName = "dialect", isAttribute = true)
+    val dialect: Int,
+
+    @JacksonXmlElementWrapper(localName = "enums")
+    val enums: List<EnumXml>,
+
+    @JacksonXmlElementWrapper(localName = "messages")
+    val messages: List<MessageXml>
+) {
+    val includes = mutableListOf<String>()
+
+    @JsonSetter("include")
+    fun addInclude(include: String) {
+        includes += include
+    }
+
+    fun toModel(name: String) = MavlinkMod(
+        name,
+        includes,
+        version,
+        dialect,
+        enums.map { it.toModel() },
+        messages.map { it.toModel() }
+    )
+}
