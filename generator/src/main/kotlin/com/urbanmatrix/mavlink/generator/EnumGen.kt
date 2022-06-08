4b3e5940f447bb4d619d43c54d75c446c7e6e2fa
divyanshu <divyanshu.pundir@gmail.com>
2022-06-08 15:41:17 +0530

    feat: Added basic enum generation

---
+++ generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/EnumGen.kt
@@ -0,0 +1,39 @@
+package com.urbanmatrix.mavlink.generator
+
+import com.squareup.kotlinpoet.*
+import com.urbanmatrix.mavlink.api.MavEnum
+import com.urbanmatrix.mavlink.generator.models.EnumEntryModel
+import com.urbanmatrix.mavlink.generator.models.EnumModel
+import kotlin.Long
+
+
+fun EnumModel.generateFileSpec(packageName: String): FileSpec {
+    val enum = TypeSpec.enumBuilder(CaseFormat.fromSnake(name).toUpperCamel())
+        .addSuperinterface(MavEnum::class)
+        .primaryConstructor(
+            FunSpec.constructorBuilder()
+                .addParameter("value", Long::class)
+                .build()
+        )
+        .addProperty(
+            PropertySpec.builder("value", Long::class, KModifier.OVERRIDE)
+                .initializer("value")
+                .build()
+        ).apply {
+            entries.forEach {
+                addEnumConstant(it.name, it.generateTypeSpec())
+            }
+        }
+        .build()
+
+    return FileSpec.builder(packageName, CaseFormat.fromSnake(name).toUpperCamel())
+        .addType(enum)
+        .build()
+}
+
+fun EnumEntryModel.generateTypeSpec(): TypeSpec {
+    return TypeSpec
+        .anonymousClassBuilder()
+        .addSuperclassConstructorParameter("%LL", value)
+        .build()
+}
