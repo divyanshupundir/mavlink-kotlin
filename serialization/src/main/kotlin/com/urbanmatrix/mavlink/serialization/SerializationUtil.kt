67b1e6dcd7f060c3d49a2f13ee413c1b47f98f13
divyanshu <divyanshu.pundir@gmail.com>
2022-06-09 18:30:25 +0530

    fix: Serialization array index out of bounds check

--- serialization/src/main/kotlin/com/urbanmatrix/mavlink/serialization/SerializationUtil.kt
+++ serialization/src/main/kotlin/com/urbanmatrix/mavlink/serialization/SerializationUtil.kt
@@ -50,67 +50,82 @@ fun ByteBuffer.encodeString(value: String, length: Int) {
 fun ByteBuffer.encodeUint8Array(arr: List<Int>, dataSize: Int) = encodeArray(
     arr,
     dataSize / Byte.SIZE_BYTES,
+    0,
     ByteBuffer::encodeUint8
 )
 
 fun ByteBuffer.encodeInt8Array(arr: List<Int>, dataSize: Int) = encodeArray(
     arr,
     dataSize / Byte.SIZE_BYTES,
+    0,
     ByteBuffer::encodeInt8
 )
 
 fun ByteBuffer.encodeUint16Array(arr: List<Int>, dataSize: Int) = encodeArray(
     arr,
     dataSize / Short.SIZE_BYTES,
+    0,
     ByteBuffer::encodeUint16
 )
 
 fun ByteBuffer.encodeInt16Array(arr: List<Int>, dataSize: Int) = encodeArray(
     arr,
     dataSize / Short.SIZE_BYTES,
+    0,
     ByteBuffer::encodeInt16
 )
 
 fun ByteBuffer.encodeUint32Array(arr: List<Long>, dataSize: Int) = encodeArray(
     arr,
     dataSize / Int.SIZE_BYTES,
+    0L,
     ByteBuffer::encodeUint32
 )
 
 fun ByteBuffer.encodeInt32Array(arr: List<Int>, dataSize: Int) = encodeArray(
     arr,
     dataSize / Int.SIZE_BYTES,
+    0,
     ByteBuffer::encodeInt32
 )
 
 fun ByteBuffer.encodeUint64Array(arr: List<BigInteger>, dataSize: Int) = encodeArray(
     arr,
     dataSize / Long.SIZE_BYTES,
+    BigInteger.ZERO,
     ByteBuffer::encodeUint64
 )
 
 fun ByteBuffer.encodeInt64Array(arr: List<Long>, dataSize: Int) = encodeArray(
     arr,
     dataSize / Long.SIZE_BYTES,
+    0L,
     ByteBuffer::encodeInt64
 )
 
 fun ByteBuffer.encodeFloatArray(arr: List<Float>, dataSize: Int) = encodeArray(
     arr,
     dataSize / Float.SIZE_BYTES,
+    0F,
     ByteBuffer::encodeFloat
 )
 
 fun ByteBuffer.encodeDoubleArray(arr: List<Double>, dataSize: Int) = encodeArray(
     arr,
     dataSize / Double.SIZE_BYTES,
+    0.0,
     ByteBuffer::encodeDouble
 )
 
 fun ByteBuffer.encodeEnumValue(value: Long, length: Int): Unit = encodeIntegerValue(value, length)
 
-private inline fun <T : Any> ByteBuffer.encodeArray(arr: List<T>, elementCount: Int, encode: ByteBuffer.(T) -> Unit) {
-    for (i in 0 until elementCount) this.encode(arr[i])
+private inline fun <T : Any> ByteBuffer.encodeArray(
+    arr: List<T>,
+    elementCount: Int,
+    defaultValue: T,
+    encode: ByteBuffer.(T) -> Unit
+) {
+    for (i in 0 until elementCount) this.encode(if (i < arr.size) arr[i] else defaultValue)
 }
 
 private fun ByteBuffer.encodeIntegerValue(value: Long, length: Int) {
