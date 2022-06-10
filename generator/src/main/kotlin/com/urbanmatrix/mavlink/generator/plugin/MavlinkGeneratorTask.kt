04a1685be515e5a771490c101dbcf2a44291c371
divyanshu <divyanshu.pundir@gmail.com>
2022-06-10 15:11:59 +0530

    feat: Stricter mavlink definition checks

--- generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/plugin/MavlinkGeneratorTask.kt
+++ generator/src/main/kotlin/com/urbanmatrix/mavlink/generator/plugin/MavlinkGeneratorTask.kt
@@ -4,6 +4,7 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import java.io.File
+import java.io.IOException
 
 abstract class MavlinkGeneratorTask : DefaultTask() {
 
@@ -16,30 +17,43 @@ abstract class MavlinkGeneratorTask : DefaultTask() {
 
     private val definitions = mutableSetOf<File>()
 
-    fun include(file: File){
-        definitions.add(file)
+    fun include(file: File) {
+        definitions += file
+    }
+
+    fun includeDir(dir: File) {
+        definitions += dir.listFiles()!!.toSet()
     }
 
     @TaskAction
     fun generate() {
         if (definitions.isEmpty())
-            throw IllegalStateException("No XML definition provided.")
+            throw IllegalArgumentException("No XML definition provided.")
 
         for (def in definitions) if (!def.isFile)
-            throw IllegalStateException("$def is not a file.")
+            throw IllegalArgumentException("Not a file: $def")
+
+        for (def in definitions) if (def.extension != "xml")
+            throw IllegalArgumentException("Not a mavlink definition file: $def")
 
         if (!this::generatedSourcesDir.isInitialized)
-            throw IllegalStateException("No generated source directory provided.")
+            throw IllegalArgumentException("No generated source directory provided.")
 
         if (!generatedSourcesDir.isDirectory)
-            throw IllegalStateException("Generated sources directory should be a directory.")
+            throw IllegalArgumentException("Generated sources directory should be a directory.")
 
         if (generatedSourcesDir.exists() && !generatedSourcesDir.deleteRecursively())
-            throw IllegalStateException("Cannot delete generated sources directory.")
+            throw IllegalArgumentException("Cannot delete generated sources directory.")
 
         if (!generatedSourcesDir.mkdirs())
-            throw IllegalStateException("Cannot create generated sources directory.")
+            throw IllegalArgumentException("Cannot create generated sources directory.")
 
-        val mavlinkModels = definitions.map { MavlinkDefinitionParser.parse(it) }
+        val mavlinkModels = definitions.map {
+            try {
+                MavlinkDefinitionParser.parse(it)
+            } catch (e: IOException) {
+                throw IllegalArgumentException("Invalid mavlink schema: $it")
+            }
+        }
     }
 }
