4034ca231d2c21975a6fca7369b8e5f14a61b45f
divyanshu <divyanshu.pundir@gmail.com>
2022-06-15 18:36:45 +0530

    feat: Added AbstractMavDialect to api

---
+++ api/src/main/kotlin/com/urbanmatrix/mavlink/api/AbstractMavDialect.kt
@@ -0,0 +1,34 @@
+package com.urbanmatrix.mavlink.api
+
+abstract class AbstractMavlinkDialect(
+    private val dependencies: Set<MavDialect>,
+    private val messages: Map<Int, MavMessage.Metadata<out MavMessage<*>>>
+) : MavDialect {
+
+    override fun resolveMetadataOrNull(messageId: Int): MavMessage.Metadata<out MavMessage<*>>? {
+        if (messages.containsKey(messageId)) {
+            return messages[messageId]
+        } else {
+            for (dependency in dependencies) {
+                val result = dependency.resolveMetadataOrNull(messageId)
+                if (result != null) {
+                    return result
+                }
+            }
+        }
+        return null
+    }
+
+    override fun supports(messageId: Int): Boolean {
+        if (messages.containsKey(messageId)) {
+            return true
+        } else {
+            for (dependency in dependencies) {
+                if (dependency.supports(messageId)) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+}
