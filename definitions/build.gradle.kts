8d8f1efcb6b42b7f5e1f3b445a9aa7af27a456b6
divyanshu <divyanshu.pundir@gmail.com>
2022-07-16 11:43:55 +0530

    feat: Added definitions subproject and moved generated definitions to it

---
+++ definitions/build.gradle.kts
@@ -0,0 +1,40 @@
+buildscript {
+    dependencies {
+        classpath("${Specs.group}:generator:${Specs.Plugin.releaseVersion}")
+    }
+}
+
+plugins {
+    kotlin("jvm")
+    id("${Specs.group}.generator") version Specs.Plugin.releaseVersion
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+val genSrcDir = file("src/main/kotlin")
+
+tasks.getByName<com.urbanmatrix.mavlink.generator.plugin.MavlinkGeneratorTask>("generateMavlink") {
+    include(file("mavlink/message_definitions/v1.0/minimal.xml"))
+    include(file("mavlink/message_definitions/v1.0/common.xml"))
+    include(file("mavlink/message_definitions/v1.0/ardupilotmega.xml"))
+    include(file("mavlink/message_definitions/v1.0/ASLUAV.xml"))
+    include(file("mavlink/message_definitions/v1.0/AVSSUAS.xml"))
+    include(file("mavlink/message_definitions/v1.0/icarous.xml"))
+    include(file("mavlink/message_definitions/v1.0/matrixpilot.xml"))
+    include(file("mavlink/message_definitions/v1.0/paparazzi.xml"))
+    include(file("mavlink/message_definitions/v1.0/standard.xml"))
+    include(file("mavlink/message_definitions/v1.0/ualberta.xml"))
+    include(file("mavlink/message_definitions/v1.0/uAvionix.xml"))
+
+    generatedSourcesDir = genSrcDir
+}
+
+dependencies {
+    implementation(Deps.mavlinkKotlinApi)
+    implementation(Deps.mavlinkKotlinSerialization)
+
+    testImplementation(TestDeps.jupiterApi)
+    testRuntimeOnly(TestDeps.jupiterEngine)
+}
--- mavlink-kotlin/build.gradle.kts
+++ mavlink-kotlin/build.gradle.kts
@@ -1,40 +1,9 @@
-buildscript {
-    dependencies {
-        classpath("${Specs.group}:generator:${Specs.Plugin.releaseVersion}")
-    }
-}
-
 plugins {
     kotlin("jvm")
-    id("${Specs.group}.generator") version Specs.Plugin.releaseVersion
-    idea
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
-}
-
-tasks.getByName<com.urbanmatrix.mavlink.generator.plugin.MavlinkGeneratorTask>("generateMavlink") {
-    include(file("mavlink/message_definitions/v1.0/minimal.xml"))
-    include(file("mavlink/message_definitions/v1.0/common.xml"))
-    include(file("mavlink/message_definitions/v1.0/ardupilotmega.xml"))
-    include(file("mavlink/message_definitions/v1.0/ASLUAV.xml"))
-    include(file("mavlink/message_definitions/v1.0/AVSSUAS.xml"))
-    include(file("mavlink/message_definitions/v1.0/icarous.xml"))
-    include(file("mavlink/message_definitions/v1.0/matrixpilot.xml"))
-    include(file("mavlink/message_definitions/v1.0/paparazzi.xml"))
-    include(file("mavlink/message_definitions/v1.0/standard.xml"))
-    include(file("mavlink/message_definitions/v1.0/ualberta.xml"))
-    include(file("mavlink/message_definitions/v1.0/uAvionix.xml"))
-
-    generatedSourcesDir = file("src/main/kotlin-gen")
-}
-
-idea {
-    module {
-        sourceDirs.plusAssign(file("src/main/kotlin-gen"))
-        generatedSourceDirs.plusAssign(file("src/main/kotlin-gen"))
-    }
 }
 
 dependencies {
