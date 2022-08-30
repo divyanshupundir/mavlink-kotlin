e12e2811abbd89c14f0f10a221c08f815d9e27f3
divyanshu <divyanshu.pundir@gmail.com>
2022-08-30 21:17:48 +0530

    chore: Version bump

--- buildSrc/src/main/kotlin/ProjectSpecs.kt
+++ buildSrc/src/main/kotlin/ProjectSpecs.kt
@@ -2,11 +2,11 @@ object Specs {
     const val group = "xyz.urbanmatrix.mavlink"
 
     object Plugin {
-        const val releaseVersion = "0.4.4"
-        const val developmentVersion = "0.4.5"
+        const val releaseVersion = "0.4.5"
+        const val developmentVersion = "0.4.6"
     }
 
     object Lib {
-        const val developmentVersion = "0.4.5"
+        const val developmentVersion = "0.4.6"
     }
 }
