040c2cf91f47c84abef5059074890276b6f66ed0
vishesh211 <visheshtiwari211@gmail.com>
2022-09-16 15:24:55 +0530

    feat: Added coroutinesextensions

--- adapters/coroutines/src/main/kotlin/xyz/urbanmatrix/mavlink/adapters/coroutines/coroutinesextensions.kt
+++ adapters/coroutines/src/main/kotlin/xyz/urbanmatrix/mavlink/adapters/coroutines/coroutinesextensions.kt
@@ -1,2 +1,10 @@
 package xyz.urbanmatrix.mavlink.adapters.coroutines
 
+import xyz.urbanmatrix.mavlink.connection.MavConnection
+
+fun MavConnection.asCoroutines(
+    onReadEnded: () -> Unit = {}
+): CoroutinesMavConnection = CoroutinesMavConnectionImpl(
+    this,
+    onReadEnded
+)
