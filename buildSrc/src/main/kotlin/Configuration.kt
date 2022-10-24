5a62fbfc15b83ee680f3bc3bbc04ec16b34ffdbd
divyanshu <divyanshu.pundir@gmail.com>
2022-10-24 22:00:30 +0530

    chore: Update version

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -2,11 +2,11 @@ object Config {
     const val group = "xyz.urbanmatrix.mavlink"
 
     object Plugin {
-        const val releaseVersion = "0.7.0"
-        const val developmentVersion = "0.7.1"
+        const val releaseVersion = "0.8.0"
+        const val developmentVersion = "0.8.1"
     }
 
     object Lib {
-        const val developmentVersion = "0.7.1"
+        const val developmentVersion = "0.8.1"
     }
 }
