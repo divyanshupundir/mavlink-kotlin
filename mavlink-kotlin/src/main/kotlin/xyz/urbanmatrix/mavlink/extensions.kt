1609fcb83f735e6a2eb8fb2032995e545eb40bde
divyanshu1234 <divyanshu.pundir@gmail.com>
2022-11-09 20:56:12 +0530

    feat: Add wrap functions for MavBitmask

--- mavlink-kotlin/src/main/kotlin/xyz/urbanmatrix/mavlink/extensions.kt
+++ mavlink-kotlin/src/main/kotlin/xyz/urbanmatrix/mavlink/extensions.kt
@@ -1,10 +1,16 @@
 package xyz.urbanmatrix.mavlink
 
+import xyz.urbanmatrix.mavlink.api.MavBitmask
+import xyz.urbanmatrix.mavlink.api.MavBitmaskValue
 import xyz.urbanmatrix.mavlink.api.MavEnum
 import xyz.urbanmatrix.mavlink.api.MavEnumValue
 import xyz.urbanmatrix.mavlink.raw.MavRawFrameReader
 import java.io.InputStream
 
 fun <E : MavEnum> E.wrap() = MavEnumValue.of(this)
+
+fun <E : MavBitmask> E.wrap() = MavBitmaskValue.of(this)
+
+fun <E : MavBitmask> List<E>.wrap() = MavBitmaskValue.of(this)
 
 fun InputStream.mavRawFrameReader() = MavRawFrameReader(this)
