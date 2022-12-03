8f27699ec934819250a05759a2b04c239f58fa01
divyanshu1234 <divyanshu.pundir@gmail.com>
2022-12-03 22:14:08 +0530

    chore: Plugin version bump

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -2,11 +2,11 @@ object Config {
     const val group = "xyz.urbanmatrix.mavlink"
 
     object Plugin {
-        const val releaseVersion = "0.11.0"
-        const val developmentVersion = "0.11.1"
+        const val releaseVersion = "0.12.0"
+        const val developmentVersion = "0.12.1"
     }
 
     object Lib {
-        const val developmentVersion = "0.11.1"
+        const val developmentVersion = "0.12.0"
     }
 }
