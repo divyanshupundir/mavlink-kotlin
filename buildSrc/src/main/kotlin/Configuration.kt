773409c5b5259d56678f49c566cbdb3a147545ed
divyanshupundir <divyanshu.pundir@gmail.com>
2023-05-14 23:30:50 +0530

    chore: Dev version bump

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -6,10 +6,10 @@ object Config {
 
     object Plugin {
         const val releaseVersion = "0.17.0"
-        const val developmentVersion = "0.17.1"
+        const val developmentVersion = "0.18.0-alpha"
     }
 
     object Lib {
-        const val developmentVersion = "0.17.1"
+        const val developmentVersion = "0.18.0-alpha"
     }
 }
