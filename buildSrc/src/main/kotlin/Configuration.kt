927f97ff66455fb9fae44c3701b7247b7ae6f0eb
divyanshupundir <divyanshu.pundir@gmail.com>
2023-06-23 11:13:51 +0530

    chore: Version bump to 0.20.0-alpha

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -6,10 +6,10 @@ object Config {
 
     object Plugin {
         const val releaseVersion = "0.19.0"
-        const val developmentVersion = "0.19.1"
+        const val developmentVersion = "0.20.0-alpha"
     }
 
     object Lib {
-        const val developmentVersion = "0.19.1"
+        const val developmentVersion = "0.20.0-alpha"
     }
 }
