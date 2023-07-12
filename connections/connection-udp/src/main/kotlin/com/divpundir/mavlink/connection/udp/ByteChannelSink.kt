034ee39c31dde625db640d92ce3bcdf1b770a979
divyanshupundir <divyanshu.pundir@gmail.com>
2023-07-12 23:11:08 +0530

    feat: Add ByteChannelSource and ByteChannelSink

---
+++ connections/connection-udp/src/main/kotlin/com/divpundir/mavlink/connection/udp/ByteChannelSink.kt
@@ -0,0 +1,50 @@
+package com.divpundir.mavlink.connection.udp
+
+import okio.Buffer
+import okio.Sink
+import okio.Timeout
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.channels.WritableByteChannel
+import kotlin.math.min
+
+internal class ByteChannelSink(
+    private val channel: WritableByteChannel,
+    private val timeout: Timeout
+) : Sink {
+
+    private val cursor: Buffer.UnsafeCursor = Buffer.UnsafeCursor()
+
+    @Throws(IOException::class)
+    override fun write(source: Buffer, byteCount: Long) {
+        check(channel.isOpen) { "closed" }
+
+        if (byteCount == 0L) {
+            return
+        }
+
+        var remaining = byteCount
+        while (remaining > 0) {
+            timeout.throwIfReached()
+
+            source.readUnsafe(cursor).use { _ ->
+                cursor.seek(0)
+
+                val length = min(cursor.end - cursor.start, remaining.toInt())
+                val written = channel.write(ByteBuffer.wrap(cursor.data, cursor.start, length)).toLong()
+
+                remaining -= written
+                source.skip(written)
+            }
+        }
+    }
+
+    override fun flush() {}
+
+    override fun timeout(): Timeout = timeout
+
+    @Throws(IOException::class)
+    override fun close() {
+        channel.close()
+    }
+}
