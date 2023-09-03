a7d3151f46dfad939adacddfafc2a874d6235eaf
divyanshupundir <divyanshu.pundir@gmail.com>
2023-09-03 13:04:03 +0530

    test: Update EnumXmlTest

--- generator/src/test/kotlin/com/divpundir/mavlink/generator/models/EnumXmlTest.kt
+++ generator/src/test/kotlin/com/divpundir/mavlink/generator/models/EnumXmlTest.kt
@@ -2,6 +2,7 @@ package com.divpundir.mavlink.generator.models
 
 import org.junit.jupiter.api.Test
 import com.divpundir.mavlink.generator.createXmlMapper
+import org.junit.jupiter.api.Assertions.assertEquals
 
 class EnumXmlTest {
 
@@ -57,7 +58,130 @@ class EnumXmlTest {
         </enum>
         """.trimIndent()
 
-        println(mapper.readValue(inp, EnumXml::class.java).toModel())
+        val expected = EnumModel(
+            name = "HL_FAILURE_FLAG",
+            bitmask = true,
+            entries = listOf(
+                EnumEntryModel(
+                    value = 1,
+                    name = "HL_FAILURE_FLAG_GPS",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "GPS failure."
+                ),
+                EnumEntryModel(
+                    value = 2,
+                    name = "HL_FAILURE_FLAG_DIFFERENTIAL_PRESSURE",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Differential pressure sensor failure."
+                ),
+                EnumEntryModel(
+                    value = 4,
+                    name = "HL_FAILURE_FLAG_ABSOLUTE_PRESSURE",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Absolute pressure sensor failure."
+                ),
+                EnumEntryModel(
+                    value = 8,
+                    name = "HL_FAILURE_FLAG_3D_ACCEL",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Accelerometer sensor failure."
+                ),
+                EnumEntryModel(
+                    value = 16,
+                    name = "HL_FAILURE_FLAG_3D_GYRO",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Gyroscope sensor failure."
+                ),
+                EnumEntryModel(
+                    value = 32,
+                    name = "HL_FAILURE_FLAG_3D_MAG",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Magnetometer sensor failure."
+                ),
+                EnumEntryModel(
+                    value = 64,
+                    name = "HL_FAILURE_FLAG_TERRAIN",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Terrain subsystem failure."
+                ),
+                EnumEntryModel(
+                    value = 128,
+                    name = "HL_FAILURE_FLAG_BATTERY",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Battery failure/critical low battery."
+                ),
+                EnumEntryModel(
+                    value = 256,
+                    name = "HL_FAILURE_FLAG_RC_RECEIVER",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "RC receiver failure/no rc connection."
+                ),
+                EnumEntryModel(
+                    value = 512,
+                    name = "HL_FAILURE_FLAG_OFFBOARD_LINK",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Offboard link failure."
+                ),
+                EnumEntryModel(
+                    value = 1024,
+                    name = "HL_FAILURE_FLAG_ENGINE",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Engine failure."
+                ),
+                EnumEntryModel(
+                    value = 2048,
+                    name = "HL_FAILURE_FLAG_GEOFENCE",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Geofence violation."
+                ),
+                EnumEntryModel(
+                    value = 4096,
+                    name = "HL_FAILURE_FLAG_ESTIMATOR",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Estimator failure, for example measurement rejection or large variances."
+                ),
+                EnumEntryModel(
+                    value = 8192,
+                    name = "HL_FAILURE_FLAG_MISSION",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Mission failure."
+                )
+            ),
+            deprecated = null,
+            description = "Flags to report failure cases over the high latency telemtry."
+        )
+
+        val actual = mapper.readValue(inp, EnumXml::class.java).toModel()
+
+        assertEquals(expected, actual)
     }
 
 
@@ -111,7 +235,130 @@ class EnumXmlTest {
         </enum>
         """.trimIndent()
 
-        println(mapper.readValue(inp, EnumXml::class.java).toModel())
+        val expected = EnumModel(
+            name = "GIMBAL_MANAGER_CAP_FLAGS",
+            bitmask = true,
+            entries = listOf(
+                EnumEntryModel(
+                    value = 1,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_HAS_RETRACT",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_HAS_RETRACT."
+                ),
+                EnumEntryModel(
+                    value = 2,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_HAS_NEUTRAL",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_HAS_NEUTRAL."
+                ),
+                EnumEntryModel(
+                    value = 4,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_HAS_ROLL_AXIS",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_HAS_ROLL_AXIS."
+                ),
+                EnumEntryModel(
+                    value = 8,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_HAS_ROLL_FOLLOW",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_HAS_ROLL_FOLLOW."
+                ),
+                EnumEntryModel(
+                    value = 16,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_HAS_ROLL_LOCK",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_HAS_ROLL_LOCK."
+                ),
+                EnumEntryModel(
+                    value = 32,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_HAS_PITCH_AXIS",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_HAS_PITCH_AXIS."
+                ),
+                EnumEntryModel(
+                    value = 64,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_HAS_PITCH_FOLLOW",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_HAS_PITCH_FOLLOW."
+                ),
+                EnumEntryModel(
+                    value = 128,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_HAS_PITCH_LOCK",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_HAS_PITCH_LOCK."
+                ),
+                EnumEntryModel(
+                    value = 256,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_HAS_YAW_AXIS",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_HAS_YAW_AXIS."
+                ),
+                EnumEntryModel(
+                    value = 512,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_HAS_YAW_FOLLOW",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_HAS_YAW_FOLLOW."
+                ),
+                EnumEntryModel(
+                    value = 1024,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_HAS_YAW_LOCK",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_HAS_YAW_LOCK."
+                ),
+                EnumEntryModel(
+                    value = 2048,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_SUPPORTS_INFINITE_YAW",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Based on GIMBAL_DEVICE_CAP_FLAGS_SUPPORTS_INFINITE_YAW."
+                ),
+                EnumEntryModel(
+                    value = 65536,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_CAN_POINT_LOCATION_LOCAL",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Gimbal manager supports to point to a local position."
+                ),
+                EnumEntryModel(
+                    value = 131072,
+                    name = "GIMBAL_MANAGER_CAP_FLAGS_CAN_POINT_LOCATION_GLOBAL",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Gimbal manager supports to point to a global latitude, longitude, altitude position."
+                )
+            ),
+            deprecated = null,
+            description = "Gimbal manager high level capability flags (bitmap). The first 16 bits are identical to the GIMBAL_DEVICE_CAP_FLAGS. However, the gimbal manager does not need to copy the flags from the gimbal but can also enhance the capabilities and thus add flags."
+        )
+
+        val actual = mapper.readValue(inp, EnumXml::class.java).toModel()
+
+        assertEquals(expected, actual)
     }
 
     @Test
@@ -133,6 +380,49 @@ class EnumXmlTest {
         </enum>
         """.trimIndent()
 
-        println(mapper.readValue(inp, EnumXml::class.java).toModel())
+        val expected = EnumModel(
+            name = "FENCE_BREACH",
+            bitmask = false,
+            entries = listOf(
+                EnumEntryModel(
+                    value = 0,
+                    name = "FENCE_BREACH_NONE",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "No last fence breach"
+                ),
+                EnumEntryModel(
+                    value = 1,
+                    name = "FENCE_BREACH_MINALT",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Breached minimum altitude"
+                ),
+                EnumEntryModel(
+                    value = 2,
+                    name = "FENCE_BREACH_MAXALT",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Breached maximum altitude"
+                ),
+                EnumEntryModel(
+                    value = 3,
+                    name = "FENCE_BREACH_BOUNDARY",
+                    params = emptyList(),
+                    workInProgress = false,
+                    deprecated = null,
+                    description = "Breached fence boundary"
+                )
+            ),
+            deprecated = null,
+            description = null
+        )
+
+        val actual = mapper.readValue(inp, EnumXml::class.java).toModel()
+
+        assertEquals(expected, actual)
     }
 }
