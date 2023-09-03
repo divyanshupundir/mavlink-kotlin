b612a130c11432aba4b8a944d646341872b88c0d
divyanshupundir <divyanshu.pundir@gmail.com>
2023-09-03 12:50:21 +0530

    test: Update DeprecatedXmlTest

--- generator/src/test/kotlin/com/divpundir/mavlink/generator/models/DeprecatedXmlTest.kt
+++ generator/src/test/kotlin/com/divpundir/mavlink/generator/models/DeprecatedXmlTest.kt
@@ -1,6 +1,7 @@
 package com.divpundir.mavlink.generator.models
 
 import com.divpundir.mavlink.generator.createXmlMapper
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class DeprecatedXmlTest {
@@ -10,18 +11,46 @@ class DeprecatedXmlTest {
     @Test
     fun withContent() {
         val inp = "<deprecated since=\"2020-01\" replaced_by=\"MAV_CMD_DO_GIMBAL_MANAGER_CONFIGURE\">This message has been superseded by MAV_CMD_DO_GIMBAL_MANAGER_CONFIGURE. The message can still be used to communicate with legacy gimbals implementing it.</deprecated>"
-        println(mapper.readValue(inp, DeprecatedXml::class.java).toModel())
+
+        val expected = DeprecatedModel(
+            since = "2020-01",
+            replacedBy = "MAV_CMD_DO_GIMBAL_MANAGER_CONFIGURE",
+            content = "This message has been superseded by MAV_CMD_DO_GIMBAL_MANAGER_CONFIGURE. The message can still be used to communicate with legacy gimbals implementing it."
+        )
+
+        val actual = mapper.readValue(inp, DeprecatedXml::class.java).toModel()
+
+        assertEquals(expected, actual)
     }
 
     @Test
     fun emptyContent() {
         val inp = "<deprecated since=\"2020-01\" replaced_by=\"MAV_CMD_DO_GIMBAL_MANAGER_CONFIGURE\"></deprecated>"
-        println(mapper.readValue(inp, DeprecatedXml::class.java).toModel())
+
+        val expected = DeprecatedModel(
+            since = "2020-01",
+            replacedBy = "MAV_CMD_DO_GIMBAL_MANAGER_CONFIGURE",
+            content = null
+        )
+
+        val actual = mapper.readValue(inp, DeprecatedXml::class.java).toModel()
+
+        assertEquals(expected, actual)
+
     }
 
     @Test
     fun withoutContent() {
         val inp = "<deprecated since=\"2020-01\" replaced_by=\"MAV_CMD_DO_GIMBAL_MANAGER_CONFIGURE\"/>"
-        println(mapper.readValue(inp, DeprecatedXml::class.java).toModel())
+
+        val expected = DeprecatedModel(
+            since = "2020-01",
+            replacedBy = "MAV_CMD_DO_GIMBAL_MANAGER_CONFIGURE",
+            content = null
+        )
+
+        val actual = mapper.readValue(inp, DeprecatedXml::class.java).toModel()
+
+        assertEquals(expected, actual)
     }
 }
