0df99ba83678cfbb091a0f29549383b3069f88d0
divyanshupundir <divyanshu.pundir@gmail.com>
2023-09-03 22:12:36 +0530

    test: Update ParamXmlTest

--- generator/src/test/kotlin/com/divpundir/mavlink/generator/models/ParamXmlTest.kt
+++ generator/src/test/kotlin/com/divpundir/mavlink/generator/models/ParamXmlTest.kt
@@ -1,6 +1,7 @@
 package com.divpundir.mavlink.generator.models
 
 import com.divpundir.mavlink.generator.createXmlMapper
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class ParamXmlTest {
@@ -10,12 +11,32 @@ class ParamXmlTest {
     @Test
     fun withContent() {
         val inp = "<param index=\"7\" label=\"Engine Cut-off\" units=\"ds\" minValue=\"0\" increment=\"1\">Main engine cut-off time before camera trigger. (0 means no cut-off)</param>"
-        println(mapper.readValue(inp, ParamXml::class.java).toModel())
+
+        val expected = ParamModel(
+            index = 7,
+            label = "Engine Cut-off",
+            units = "ds",
+            content = "Main engine cut-off time before camera trigger. (0 means no cut-off)"
+        )
+
+        val actual = mapper.readValue(inp, ParamXml::class.java).toModel()
+
+        assertEquals(expected, actual)
     }
 
     @Test
     fun withoutLabel() {
         val inp = "<param index=\"7\" units=\"ds\" minValue=\"0\" increment=\"1\">Main engine cut-off time before camera trigger. (0 means no cut-off)</param>"
-        println(mapper.readValue(inp, ParamXml::class.java).toModel())
+
+        val expected = ParamModel(
+            index = 7,
+            label = null,
+            units = "ds",
+            content = "Main engine cut-off time before camera trigger. (0 means no cut-off)"
+        )
+
+        val actual = mapper.readValue(inp, ParamXml::class.java).toModel()
+
+        assertEquals(expected, actual)
     }
 }
