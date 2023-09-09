9a6b8553974811360413369589dde1a9b5c23412
divyanshupundir <divyanshu.pundir@gmail.com>
2023-09-09 19:30:30 +0530

    docs: Add StreamState docs

--- connections/connection-core/src/main/kotlin/com/divpundir/mavlink/connection/StreamState.kt
+++ connections/connection-core/src/main/kotlin/com/divpundir/mavlink/connection/StreamState.kt
@@ -1,13 +1,30 @@
 package com.divpundir.mavlink.connection
 
+/**
+ * Represents the state of a [MavConnection] stream.
+ */
 public sealed interface StreamState {
 
+    /**
+     * Represents the active state of the [MavConnection] stream.
+     *
+     * The connection is in the opened state and data is being read.
+     */
     public data object Active : StreamState
 
+    /**
+     * Represents the inactive state of the [MavConnection] stream.
+     */
     public sealed interface Inactive : StreamState {
 
+        /**
+         * The connection is in the closed state. Either it was not opened or was closed safely by the user.
+         */
         public data object Stopped : Inactive
 
+        /**
+         * The connection failed after it was opened.
+         */
         public data class Failed(val cause: Throwable) : Inactive
     }
 }
