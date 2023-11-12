5ae81763dd02b83a4e089825f126a6155ad2f2ce
divyanshupundir <divyanshu.pundir@gmail.com>
2023-11-12 16:08:04 +0530

    feat: Implement MavDataEncoder for non-JVM sources

--- serialization/src/nonJvmMain/kotlin/com/divpundir/mavlink/serialization/NonJvmMavDataEncoder.kt
+++ serialization/src/nonJvmMain/kotlin/com/divpundir/mavlink/serialization/NonJvmMavDataEncoder.kt
@@ -2,30 +2,44 @@ package com.divpundir.mavlink.serialization
 
 private class NonJvmMavDataEncoder(size: Int) : MavDataEncoder {
 
-    override val bytes: ByteArray
-        get() = TODO("Not yet implemented")
+    override val bytes: ByteArray = ByteArray(size)
+
+    private var position = 0
 
     override val remaining: Int
-        get() = TODO("Not yet implemented")
+        get() = bytes.size - position
 
     override fun encodeByte(value: Byte) {
-        TODO("Not yet implemented")
+        bytes[position++] = value
     }
 
     override fun encodeShort(value: Short) {
-        TODO("Not yet implemented")
+        bytes[position++] = value.toByte()
+        bytes[position++] = (value.toInt() shr 8).toByte()
     }
 
     override fun encodeInt(value: Int) {
-        TODO("Not yet implemented")
+        bytes[position++] = value.toByte()
+        bytes[position++] = (value shr 8).toByte()
+        bytes[position++] = (value shr 16).toByte()
+        bytes[position++] = (value shr 24).toByte()
     }
 
     override fun encodeLong(value: Long) {
-        TODO("Not yet implemented")
+        bytes[position++] = value.toByte()
+        bytes[position++] = (value shr 8).toByte()
+        bytes[position++] = (value shr 16).toByte()
+        bytes[position++] = (value shr 24).toByte()
+        bytes[position++] = (value shr 32).toByte()
+        bytes[position++] = (value shr 40).toByte()
+        bytes[position++] = (value shr 48).toByte()
+        bytes[position++] = (value shr 56).toByte()
     }
 
     override fun encodeByteArray(src: ByteArray, offset: Int, length: Int) {
-        TODO("Not yet implemented")
+        for (i in offset until offset + length) {
+            bytes[position++] = src[i]
+        }
     }
 }
 
