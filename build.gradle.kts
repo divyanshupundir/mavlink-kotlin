8df06b84de5d7f2ae93b305a8167886921760dcc
divyanshupundir <divyanshu.pundir@gmail.com>
2023-11-19 12:01:44 +0530

    build: Update jvmargs

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.mavenpublish) apply false
-    alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.dokka)
 }
 
 allprojects {
