63819d2ca269e3a395c455c78e30a1510019e1dd
divyanshupundir <divyanshu.pundir@gmail.com>
2023-11-19 09:45:06 +0530

    fix: Update settings.gradle.kts to fix publish plugin workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
