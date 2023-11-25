dfb9759437b92e1e2c69c02be2c27d92aaf78d5c
divyanshupundir <divyanshu.pundir@gmail.com>
2023-11-25 09:57:15 +0530

    build: Add watch-os targets for library modules

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -7,14 +7,14 @@ object Config {
     val javaVersion = JavaVersion.VERSION_1_8
 
     object Plugin {
-        const val releaseVersion = "1.2.0-alpha.4"
-        const val developmentVersion = "1.2.0-alpha.5"
+        const val releaseVersion = "1.2.0-alpha.5"
+        const val developmentVersion = "1.2.0-alpha.6"
 
         const val releaseApi = "$group:api:$releaseVersion"
         const val releaseSerialization = "$group:serialization:$releaseVersion"
     }
 
     object Lib {
-        const val developmentVersion = "1.2.0-dev.3"
+        const val developmentVersion = "1.2.0-alpha.5"
     }
 }
