e7c6b68ef72ebbd8afada1f58e91f48b26b7fd7e
divyanshupundir <divyanshu.pundir@gmail.com>
2024-02-20 21:11:46 +0530

    feat: Add units to message KDoc

--- generator/src/main/kotlin/com/divpundir/mavlink/generator/FieldGen.kt
+++ generator/src/main/kotlin/com/divpundir/mavlink/generator/FieldGen.kt
@@ -10,7 +10,10 @@ import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 internal fun FieldModel.generateConstructorParameter(enumHelper: EnumHelper) = ParameterSpec
     .builder(formattedName, resolveKotlinType(enumHelper))
     .defaultValue(defaultKotlinValue(enumHelper))
-    .apply { if (content != null) addKdoc(content.replace("%", "%%")) }
+    .apply {
+        if (content != null) addKdoc(content!!.replace("%", "%%"))
+        if (units != null) addKdoc("\nunits: %L", units!!)
+    }
     .build()
 
 internal fun FieldModel.generateProperty(enumHelper: EnumHelper) = PropertySpec
