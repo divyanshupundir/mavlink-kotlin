35976c7ad1bb6a6a23543f995de8faebf8a3f621
divyanshupundir <divyanshu.pundir@gmail.com>
2024-04-14 20:15:25 +0530

    feat: Add infix operator function contains for MavBitmaskValue

--- api/src/commonMain/kotlin/com/divpundir/mavlink/api/extensions.kt
+++ api/src/commonMain/kotlin/com/divpundir/mavlink/api/extensions.kt
@@ -5,3 +5,6 @@ public fun <E : MavEnum> E.wrap(): MavEnumValue<E> = MavEnumValue.of(this)
 public fun <E : MavBitmask> E.wrap(): MavBitmaskValue<E> = MavBitmaskValue.of(this)
 
 public fun <E : MavBitmask> List<E>.wrap(): MavBitmaskValue<E> = MavBitmaskValue.of(this)
+
+public infix operator fun <E : MavBitmask> MavBitmaskValue<E>.contains(flag: E): Boolean =
+    this.value and flag.value == flag.value
