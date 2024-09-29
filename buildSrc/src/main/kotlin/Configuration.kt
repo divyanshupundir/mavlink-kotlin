38141969ca8d84096c5b66afe333589515ea88b5
divyanshupundir <divyanshu.pundir@gmail.com>
2024-09-29 21:15:04 +0530

    chore: Plugin version bump

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -4,8 +4,8 @@ object Config {
     const val group = "com.divpundir.mavlink"
 
     object Plugin {
-        const val releaseVersion = "1.2.7"
-        const val developmentVersion = "1.2.8"
+        const val releaseVersion = "1.2.8"
+        const val developmentVersion = "1.2.9-alpha"
 
         const val releaseApi = "$group:api:$releaseVersion"
         const val releaseSerialization = "$group:serialization:$releaseVersion"
