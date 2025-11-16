6ffa5e0e8c7c43f1f0c382dd6e049c9e0a61194c
divyanshupundir <divyanshu.pundir@gmail.com>
2025-11-16 22:07:04 +0530

    chore: Update plugin versions

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -4,8 +4,8 @@ object Config {
     const val group = "com.divpundir.mavlink"
 
     object Plugin {
-        const val releaseVersion = "1.2.10"
-        const val developmentVersion = "1.2.14"
+        const val releaseVersion = "1.2.14"
+        const val developmentVersion = "1.2.15"
 
         const val releaseApi = "$group:api:$releaseVersion"
         const val releaseSerialization = "$group:serialization:$releaseVersion"
